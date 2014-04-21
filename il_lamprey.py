63a15bcf7a317c874df52a81540057b4e6dba8f1
Jennifer Blight <jgblight@gmail.com>
2014-04-21 16:30:15 -0400

    working oddly well

--- il_lamprey.py
+++ il_lamprey.py
@@ -4,6 +4,8 @@ import os
 import os.path
 from math import sin,cos,pi,exp
 import subprocess
+import pickle
+import time
 
 tau = 0.1
 damp0 = -0.1
@@ -12,6 +14,8 @@ freq = 30
 
 net = nef.Network('Neural Lamprey')
 
+subprocess.call(["python","/Users/jgblight/Documents/Neuro/lamprey/pinv.py"])
+time.sleep(100)
 output = open('/Users/jgblight/Documents/Neuro/lamprey/data.pkl', 'r')
 m_d = pickle.load(output)
 m_i = pickle.load(output)
@@ -34,7 +38,7 @@ output.close()
 #net.connect('input','a0')
 
 def phi(z,m):
-	return exp(-1*(z-0.1*m)**2/0.13**2)
+	return exp(-1*((z-(1/10.0)*m)**2)/0.25)
 
 encoders = []
 for i in range(400):
@@ -43,45 +47,60 @@ for i in range(400):
 		e.append(random.choice([-1,1]))
 	encoders.append(e)
 
+def print_weights(w):
+    print w
+    return w
+
 def m_d_(x):
 	dx = []
 	for i in range(10):
 		dx0 = 0
 		for j in range(10):
-			dx0 += m_d[i][j]*x[j]
+			dx0 += (m_d[i][j])*x[j]
 		dx.append(dx0*tau + x[i])
 	return dx
 
 def m_i_(x):
 	if x[0] > 0.8:
 		o = []
-		for i in range(10):
+		for i in range(1,11):
 			dx0 = 0
-			for j in range(10):
-				dx0 += m_i[i][j]*start[j]
+			for j in range(1,11):
+				dx0 += m_i[i-1][j-1]*x[j]
 			o.append(dx0*tau)
 
 	else:
 		o = [0,0,0,0,0,0,0,0,0,0]
 	return o
 
-net.make('a', neurons=400, dimensions=10,encoders=encoders)
-net.connect('a','a',func=m_d_)
+net.make('a', neurons=400, dimensions=10,radius=20,encoders=encoders)
+net.connect('a','a',func=m_d_,weight_func=print_weights)
 
-net.make_input('input', [0])
-net.make('switch',1,1,mode='direct')
-net.connect('input','switch')
-net.connect('switch','a',func=m_i_)
+#net.make_input('input', [1])
+#net.make('switch',1,11,mode='direct')
+#net.connect('input','switch',transform=[[1],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0]])
+#e = zeros((11,10))
+#for i in range(1,11):#
+#	e[i][i-1] = 1
+#e = e.tolist()
+#print e
+#net.connect('a','switch',transform=e)
+#net.connect('switch','a',func=m_i_)
 
-def T(x):
+def T(x,z):
 	y = 0
 	for m in range(10):
-		y += x[0]*phi(0.5,m)
+		y += x[0]*phi(z,m)
 	return y
 
 net.make('T1',1,1,mode='direct')
-net.connect('a','T1',func=T)
+net.connect('a','T1',func=lambda x: T(x,0.25))
 
+net.make('T2',1,1,mode='direct')
+net.connect('a','T2',func=lambda x: T(x,0.5))
+
+net.make('T3',1,1,mode='direct')
+net.connect('a','T3',func=lambda x: T(x,0.75))
 
 net.add_to_nengo()
 
--- pinv.py
+++ pinv.py
@@ -1,24 +1,28 @@
 import numpy as np
 import sys
 import pickle
+import scipy.integrate as sp
 from math import sin,cos,pi,exp
+import time
 damp0 = -0.1
 damp = -1
 freq = 30
 
 def phi(z,m):
-	return exp(-1*(z-0.1*m)**2/0.13**2)
+	return exp(-1*np.square(z-(1/10.0)*m)/np.square(0.5))
 
 def Phi(z):
 	return [1,sin(2*pi*z),cos(2*pi*z),sin(4*pi*z)]
 
 def get_coefficient(n,m):
-	dx = 1/100000.
-	return sum([Phi(z*dx)[n]*phi(z*dx,m)*dx for z in range(0,100001)])
+	#dx = 1/100000.
+	#return sum([Phi(z*dx)[n]*phi(z*dx,m)*dx for z in range(0,100001)])
+	f = lambda x: Phi(x)[n]*phi(x,m)
+	return sp.quad(f, 0, 1)[0]
 
-Gamma = np.zeros([4,10])
+Gamma = np.zeros([3,10])
 for m in range(10):
-	for n in range(4):
+	for n in range(3):
 		Gamma[n,m] = get_coefficient(n,m)
 		#Gamma[n,m] = Phi(0.5)[n]*phi(0.5,m)
 
@@ -26,8 +30,8 @@ Gamma_inv = np.linalg.pinv(Gamma)
 
 
 
-M_d = np.array([[damp0,freq,damp0,0],[-0.5*freq,0,0.5*freq,0],[damp0,-1*freq,damp0,0],[0,0,0,damp]])
-M_i = np.array([[0.5,0,-0.5,0],[0,1,0,0],[-0.5,0,0.5,0],[0,0,0,0]])
+M_d = np.array([[damp0,freq,damp0],[-0.5*freq,0,0.5*freq],[damp0,-1*freq,damp0]])
+M_i = np.array([[0.5,0,-0.5],[0,1,0],[-0.5,0,0.5]])
 
 m_d = np.dot(np.dot(Gamma_inv,M_d),Gamma)
 m_i = np.dot(np.dot(Gamma_inv,M_i),Gamma)
@@ -35,12 +39,18 @@ m_i = np.dot(np.dot(Gamma_inv,M_i),Gamma)
 A0 = [-1 * sin(30*t*0.001) for t in range(1000)]
 A1 = [-1 * cos(30*t*0.001) for t in range(1000)]
 A2 = [sin(30*t*0.001) for t in range(1000)]
-A3 = [0 for t in range(1000)]
 
-A = np.array([A0,A1,A2,A3])
+A = np.array([A0,A1,A2])
 
 a = np.dot(Gamma_inv,A)
+print Gamma
+print ' '
 print m_d
+print ' '
+print a[:,0:100]
+#start = a[:,5]
+#sim = zeros([10,1000])
+#for i in np.arange()
 
 
 output = open('data.pkl', 'w')
